8ff3ede64ced83473c6846cb863161e56e16d463
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-21 16:37:52 -0300

    feat: add diagnostics and formatting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,28 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = augroup,
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
+}
