8da7a18de21c702c94aecb4e737769f0e0043c27
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-21 16:40:01 -0300

    feat: add {*.js|*.ts} support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,6 +28,10 @@ return {
         capabilities = capabilities,
       }
 
+      lspconfig.ts_ls.setup = {
+        capabilities = capabilities,
+      }
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
