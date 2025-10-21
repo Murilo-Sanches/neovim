42fce5fe77fffced5a311a482f360733e1f2a2d5
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-21 16:36:32 -0300

    feat: add intellisense and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,9 +21,12 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = vim.lsp.config
 
-      lspconfig.lua_ls.setup = {}
+      lspconfig.lua_ls.setup = {
+        capabilities = capabilities,
+      }
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
