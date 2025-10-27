37c57b35ccc2cddec70bab7d6e40199cdc20753d
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-26 23:20:25 -0300

    refactor: change neo-tree keymap

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,12 +3,13 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
   },
+  lazy = false,
   config = function()
     vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", {})
-    vim.keymap.set("n", "<leader>nf", ":Neotree focus<CR>", {})
+    vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
   end,
 }
