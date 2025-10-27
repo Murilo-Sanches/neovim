d2f5aed63e17695a0655ab6dab34c35d61c69aed
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-27 16:56:11 -0300

    chore: enable clipboard support for neovim on wsl

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,3 +9,20 @@ vim.g.background = "light"
 vim.opt.swapfile = false
 
 vim.wo.number = true
+
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has('wsl') == 1 then
+  local yank_group = vim.api.nvim_create_augroup('Yank', { clear = true })
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = yank_group,
+    callback = function()
+      vim.fn.system('clip.exe', vim.fn.getreg('"'))
+    end,
+  })
+
+  vim.keymap.set('i', '<C-v>', function()
+    local content = vim.fn.systemlist("powershell.exe Get-Clipboard")
+
+    vim.api.nvim_put(content, 'c', true, true)
+  end, { noremap = true, silent = true })
+end
