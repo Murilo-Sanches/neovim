4b7d060517410960d76541466d2a4e3ad96ba8d6
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-29 19:30:26 -0300

    style: prefer tabs over spaces for indentation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,7 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.o.expandtab = false
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 vim.g.mapleader = " "
 vim.g.background = "light"
