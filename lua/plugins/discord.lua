238a82f5b09bc4931b3391e11ca84ad490c554a4
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2025-10-31 02:25:12 -0300

    refactor: replace discord rich presence implementation

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,10 +1,4 @@
 return {
-	"andweeb/presence.nvim",
-	config = function()
-		require("presence").setup({
-			neovim_image_text = "Neovim",
-			main_image = "file",
-			debounce_timeout = 1,
-		})
-	end,
+	"vyfor/cord.nvim",
+	build = ":Cord update",
 }
