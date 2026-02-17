0d3ef31b6ed3ab76116d9d70a97434f7bae0f675
Murilo-Sanches <murilo.sanches.9999@outlook.com>
2026-02-17 00:01:02 -0300

    fix: resolve telescope and treesitter compatibility issues

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,6 @@ return {
 	},
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local telescope = require("telescope")
